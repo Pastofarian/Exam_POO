fd0fb82fe6307aa433a05598e1790a649dc3ebc5
AseglioChristophe <asegliochristophe@gmail.com>
2023-02-14 13:11:12 +0100

    Architecture + Data Access Services

---
+++ CarListApp/CarListApp/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Models
+{
+    public class Car
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public string Make
+        {
+            get; set;
+        }
+        public string Model
+        {
+            get; set;
+        }
+        public string Vin //vehicule identification number
+        {
+            get; set;
+        }
+    }
+}
