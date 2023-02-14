14657986e18814290800da72bf06987a0db5932b
AseglioChristophe <asegliochristophe@gmail.com>
2023-02-14 15:06:06 +0100

    Add singleton + MainPage

--- CarListApp/CarListApp/MauiProgram.cs
+++ CarListApp/CarListApp/MauiProgram.cs
@@ -1,18 +1,25 @@
-﻿namespace CarListApp;
+﻿using CarListApp.Services;
+using CarListApp.ViewModels;
+
+namespace CarListApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        builder.Services.AddSingleton<CarServices>();
+        builder.Services.AddSingleton<CarListViewModel>();
+        builder.Services.AddSingleton<MainPage>();
+
+        return builder.Build();
+    }
 }
