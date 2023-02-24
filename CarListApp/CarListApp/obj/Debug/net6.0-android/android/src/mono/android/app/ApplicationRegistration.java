9c840533947b00c293792fff5d6709a3a4e9db57
AseglioChristophe <asegliochristophe@gmail.com>
2023-02-24 13:07:25 +0100

    API Project + EntityFramework + Migration + Tools+ Sqlite build

--- CarListApp/CarListApp/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
+++ CarListApp/CarListApp/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,7 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
-		mono.android.Runtime.register ("CarListApp.MainApplication, CarListApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64bad1bb93db0dc09a.MainApplication.class, crc64bad1bb93db0dc09a.MainApplication.__md_methods);
+		mono.android.Runtime.register ("CarListApp.Maui.MainApplication, CarListApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64bdc51c9cf7ae31e4.MainApplication.class, crc64bdc51c9cf7ae31e4.MainApplication.__md_methods);
 		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
 		
 	}
